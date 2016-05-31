9d81878401119c19214b2dea1ec80327b3431e01
MARIU16 <marusanchez777@hotmail.com>
2016-05-31 19:03:46 +0200

    distintos tipos de gráficos

--- qplot Ejemplo.R
+++ qplot Ejemplo.R
@@ -19,3 +19,28 @@ qplot(x = hwy, data = mpg, fill= drv)
 ## Facets(múltiples gráficas)
 library(ggplot2)
 qplot(x = displ, y = hwy, data = mpg, facets= .~drv)
+
+## Añadimos objetos de tipo geometric
+library(ggplot2)
+qplot(x = displ, y = hwy, data = mpg, color = drv) + geom_smooth(method="lm")
+
+## Histograma
+library(ggplot2)
+qplot(x = hwy, data = mpg, fill= drv)
+
+## Facets(múltiples gráficas)
+library(ggplot2)
+qplot(x = displ, y = hwy, data = mpg, facets= .~drv)
+
+## Facets(múltiples gráficas)
+library(ggplot2)
+qplot(x = hwy,
+        data = mpg,
+        facets= drv~.,
+        binwidth= 2,
+        color = drv
+        )
+
+## Densidad
+qplot(x = hwy, data = mpg, geom= "density")
+qplot(x = hwy, data = mpg, geom= "density", color = drv)
