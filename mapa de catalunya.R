4c4f28f308aefebbd6edaba44672eec31fa3c053
MARIU16 <marusanchez777@hotmail.com>
2016-05-31 19:25:31 +0200

    Mapas con R

--- mapa de catalunya.R
+++ mapa de catalunya.R
@@ -7,3 +7,10 @@ plot(cat, lwd=10, border="skyblue", add=T)
 plot(cat,col="green4", add=T)
 grid()
 cat$data<-rnorm(1:length(cat$NAME_4), 10, 3)
+
+# Solo latitud y longitud
+library(ggmap)
+ej<-get_map(location="Mexico", source="google", maptype="terrain", zoom=5)
+ggmap(ej)
+ej<-get_map(location=c(right=-85, left=-121, bottom=13, top=33), source="osm", color="bw")
+ggmap(ej)
