cf79558aecf997ff1734f7ecec74638fff47ad1d
MARIU16 <marusanchez777@hotmail.com>
2016-05-31 20:59:30 +0200

    Inicio SSL IPBL para Suricata / Snort (agresivo)

---
+++ SSL IPBL para Suricata Snort (agresivo).R
@@ -0,0 +1,17 @@
+## Lectura de una pagina web
+con <- url("https://sslbl.abuse.ch/blacklist/sslipblacklist_aggressive.rules")
+htmlCode <- readLines(con)
+close(con)
+htmlCode
+
+## Parseando con XML
+library(XML)
+url <- "https://sslbl.abuse.ch/blacklist/sslipblacklist_aggressive.rules"
+html <- htmlTreeParse(url, useInternalNodes=TRUE)
+xpathSApply(html, "//font", xmlValue)
+
+getRow <- function(log = "") {
+  df <- data.frame()
+  
+  return(df)
+}
