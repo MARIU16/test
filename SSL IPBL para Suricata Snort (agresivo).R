232ad59f56c1f75706b083fcae143a43fb462da6
MARIU16 <marusanchez777@hotmail.com>
2016-06-02 14:39:40 +0200

    Update SSL IPBL para Suricata Snort (agresivo).R
    nuevas opciones

--- SSL IPBL para Suricata Snort (agresivo).R
+++ SSL IPBL para Suricata Snort (agresivo).R
@@ -21,3 +21,30 @@ strsplit (x=htmlCode, split=" ")
 
 #cabecera
 head(htmlCodena)
+
+#eliminar filas
+htmlCode[-8]
+
+#Separar en columnas
+dfInfo <- within(data=df, P<-data.frame (do.call('rbind',strsplit(as.character(a)," ",fixed=TRUE))))
+
+#tambien sirve y está mejor
+l <- read.table("https://sslbl.abuse.ch/blacklist/sslipblacklist_aggressive.rules", header = TRUE, sep = " ")
+
+#probar que sale la columna seleccionada
+rbind(l[6])
+
+#probar la clase del objeto
+class(l)
+
+#ver nombres de colunas
+colnames(l)
+
+#Renombrar columnas
+colnames(l)<-c("a","Protocolo","c","d","e","IP","Puerto")
+
+#quitar datos
+SC<-dfInfo[-c(1:7), ]
+
+#separar en filas
+strsplit (x=dff, split=" ")
