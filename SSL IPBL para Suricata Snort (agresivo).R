6f0e50d5447a9f72ffa0385cb204887f9cbe4cb6
MARIU16 <marusanchez777@hotmail.com>
2016-06-05 22:04:34 +0200

    Update SSL IPBL para Suricata Snort (agresivo).R

--- SSL IPBL para Suricata Snort (agresivo).R
+++ SSL IPBL para Suricata Snort (agresivo).R
@@ -179,3 +179,36 @@ tablapais<-function(de1,de2,pa,psa){
   View(tabla2)
 }
 
+#completo con país incluído
+##funciones finales 
+get_blacklist_abuse<-function() {
+  l <- read.table("https://sslbl.abuse.ch/blacklist/sslipblacklist_aggressive.rules", header = TRUE, sep = " ")
+  ip<- substring(as.character(l[,6]),2,nchar(as.character(l[,6]))-1)
+  ip_decimal<-ip_to_numeric(ip)
+  protocolo<-l[,2]
+  puerto<-l[,7]
+  tabla1<-data.frame(ip_decimal,ip,protocolo,puerto, stringsAsFactors = FALSE)
+  }
+
+get_ip_countries<-function(){
+  download.file(url="http://geolite.maxmind.com/download/geoip/database/GeoIPCountryCSV.zip", destfile="paises.zip")
+  unzip("paises.zip")
+  p <- read.table("GeoIPCountryWhois.csv",header = TRUE, sep = ",")
+  de1<-p[,3]
+  de2<-p[,4]
+  pa<-as.character(p[,5])
+  psa<-as.character(p[,6])
+  tabla2<-data.frame(de1,de2,pa,psa, stringsAsFactors = FALSE)
+}
+
+
+blacklist_countries<-function(){
+  pais<-matrix(nrow = nrow(tabla1),ncol = 1)
+  for (i in 1:nrow(tabla1)){
+    compare <- ((tabla2$de1 <= tabla1$ip_decimal[i]) & (tabla1$ip_decimal[i] <= tabla2$de2))
+    pais[i,1]<-tabla2[compare,4]
+  }
+  pais<-data.frame(pais)
+  tabla1<-cbind(tabla1,pais)
+}
+
